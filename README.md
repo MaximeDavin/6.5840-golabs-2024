16846195a99f14a28cc4e1ac3a6e12992ea5b2a5
MaximeDavin <davin.maxime@gmail.com>
2025-01-26 21:44:21 +0100

    Key/value server with no network failures

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# Lab1 MapReduce
-
+# 6.5840: Distributed Systems
 [Schedule](https://pdos.csail.mit.edu/6.824/schedule.html)\
+
+## Lab1 MapReduce
 [Lab assignment](https://pdos.csail.mit.edu/6.824/labs/lab-mr.html)
+
+## Lab2 Key/Value Server
+[Lab assignment](https://pdos.csail.mit.edu/6.824/labs/lab-kvsrv.html)
--- src/kvsrv/server.go
+++ src/kvsrv/server.go
@@ -14,30 +14,44 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type KVServer struct {
-	mu sync.Mutex
-
-	// Your definitions here.
+	mu      sync.Mutex
+	store   map[string]string
+	history map[int64]int // We store the length of the value for every transaction
 }
 
-
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	reply.Value = kv.store[args.Key]
 }
 
 func (kv *KVServer) Put(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	kv.store[args.Key] = args.Value
+	reply.Value = args.Value
 }
 
 func (kv *KVServer) Append(args *PutAppendArgs, reply *PutAppendReply) {
-	// Your code here.
+	kv.mu.Lock()
+	defer kv.mu.Unlock()
+
+	if prevValueLength, ok := kv.history[args.Id]; ok {
+		reply.Value = kv.store[args.Key][:prevValueLength]
+	} else {
+		reply.Value = kv.store[args.Key]
+		kv.history[args.Id] = len(reply.Value)
+		kv.store[args.Key] += args.Value
+
+	}
 }
 
 func StartKVServer() *KVServer {
 	kv := new(KVServer)
-
-	// You may need initialization code here.
-
+	kv.store = make(map[string]string)
+	kv.history = make(map[int64]int)
 	return kv
 }
